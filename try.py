e12339cadbd984fe8c69d24d70942f5d59f9fb62
Parthi_VTC <parthivtc06@gmail.com>
2024-09-02 13:53:14 +0530

    try py code

---
+++ try.py
@@ -0,0 +1,15 @@
+# Decimal number
+dec_num = 10
+print("Decimal Number:", dec_num)
+
+# Binary number
+bin_num = bin(dec_num)
+print("Binary Number:", bin_num)
+
+# Octal number
+oct_num = oct(dec_num)
+print("Octal Number:", oct_num)
+
+# Hexadecimal number
+hex_num = hex(dec_num)
+print("Hexadecimal Number:", hex_num)
